6eed1e016597d0df4ef85ce8e22037e6ab529610
Julio Cesar <czar.santos@outlook.com>
2022-01-27 20:04:44 -0300

    imagens e textos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+//MENU
 const $html = document.querySelector('html')
 const $checkbox = document.querySelector('#checkbox')
 
@@ -17,6 +19,36 @@ function addMenu(){
 
 btn.addEventListener('click', addMenu)
 
+
+
+//Type---------------------------------------------------
+
+    //Pega o proprio elemento 
+    function typeWriter(elemento) {
+      //Array para pegar o texto/letras
+      //metodo 'split()' divide texto em diferentes partes
+      const textoArray = elemento.innerHTML.split('');
+      elemento.innerHTML = '' //limpa html
+
+      //acessar cada letra do array
+      //'i' - index
+      textoArray.forEach((letra, i) => {
+        //animar com o tempo
+        setTimeout(() =>
+          elemento.innerHTML += letra, 100 * i)
+        //adiciona uma letra seguida da outra
+        // vezes o index
+      });
+
+
+    }
+
+    setInterval(() => { typeWriter(titulo) }, 7000);
+
+
+
+    const titulo = document.querySelector('#titulo-type')
+    typeWriter(titulo)//elemento titulo
 
 
 
